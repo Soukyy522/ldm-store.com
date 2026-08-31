058f53c232f0b0b46240ba857cb20c3de8c59858
Soukyy522 <rudigamer126@gmail.com>
2026-08-31 23:55:41 +0800

    Add files via upload

--- js/license-v2-config.js
+++ js/license-v2-config.js
@@ -4,16 +4,16 @@
         enabled:true,
         serverUrl:"https://vplweadbeujidsoponrl.supabase.co/functions/v1/ldm-license-v2",
         developerWhatsApp:"6283117590286",
-        appVersion:"26.0.0",
+        appVersion:"26.2.1",
         requestTimeoutMs:8000,
         onlineCacheMinutes:2,
         offlineGraceHours:24,
         activationPage:"license.html",
         plans:Object.freeze({
-            WARUNG_KECIL:{name:"Warung Kecil",monthly:29000,yearly:299000,devices:2,stores:1},
-            WARUNG_SEDERHANA:{name:"Warung Sederhana",monthly:59000,yearly:599000,devices:3,stores:1,trialDays:14},
-            TOKO:{name:"Toko",monthly:99000,yearly:999000,devices:10,stores:5},
-            LIFETIME:{name:"Lifetime",lifetime:3499000,devices:15,stores:8}
+            WARUNG_KECIL:{name:"Warung Kecil",monthly:69000,yearly:699000,devices:2,stores:1},
+            WARUNG_SEDERHANA:{name:"Warung Sederhana",monthly:129000,yearly:1299000,devices:3,stores:1,trialDays:14},
+            TOKO:{name:"Toko",monthly:249000,yearly:2499000,devices:10,stores:5},
+            LIFETIME:{name:"Lifetime",lifetime:7499000,devices:15,stores:8}
         })
     });
 })();
